125ea4f8bc6ddcce938bbcc8537e33d7c3e75bf0
ctlchild <ctlchild@163.com>
2019-09-10 20:13:31 +0800

    upd code

--- hw2/hello/hello.go
+++ hw2/hello/hello.go
@@ -4,10 +4,9 @@ import (
 	"fmt"
 
 	"github.com/ctlchild/SYSU_ServiceComputingOnCloud/hw2/stringutil" 
-	//"github.com/ctlchild/stringutil"
 )
 
 func main() {
-	fmt.Printf("test\n")
+//	fmt.Printf("hello world\n")
 	fmt.Printf(stringutil.Reverse("!oG ,olleH"))
 }
