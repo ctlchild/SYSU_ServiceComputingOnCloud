fa3dc48046b2eccd28b5ea902910bb86c15e1f2d
ctlchild <ctlchild@163.com>
2019-09-10 17:25:01 +0800

    add code

---
+++ hw2/hello/hello.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/ctlchild/SYSU_ServiceComputingOnCloud/hw2/stringutil" 
+	//"github.com/ctlchild/stringutil"
+)
+
+func main() {
+	fmt.Printf("test\n")
+	fmt.Printf(stringutil.Reverse("!oG ,olleH"))
+}
